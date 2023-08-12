6dff90cf41f334db7904c795fe29ab6b6745d531
ravikumar <130867166+ravikumarmalik@users.noreply.github.com>
2023-08-13 02:27:06 +0530

    Update arr1.java

--- arr1.java
+++ arr1.java
@@ -16,6 +16,23 @@ public class arr1 {
 }
 
 
+========================================================
+
+    public class arr11 {
+    //clone array
+    public static void main(String[] args) {
+        int[] num={1,2,3};
+        int[] cloneNum = num.clone();
+        // will print false as deep copy is created
+        System.out.println(num == cloneNum);
+        for (int i = 0; i < cloneNum.length; i++) {
+            System.out.print(cloneNum[i] + " ");
+        }
+    }
+}
+
+
+
 
 
 
