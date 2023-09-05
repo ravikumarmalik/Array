1925be79c73fbabec4773b91ed13f4e97feda4c4
ravikumar <130867166+ravikumarmalik@users.noreply.github.com>
2023-09-05 16:52:26 +0530

    Update arr4.java

--- arr4.java
+++ arr4.java
@@ -1,7 +1,6 @@
 public class arr4 {
     public static void main(String[] args) {
         String[] fruits={"apple","banana","mango","guava"};
-        System.out.println(fruits.length);
         for (int i=0;i<fruits.length;i++){
             System.out.print(fruits[i]+","+" ");
         }
